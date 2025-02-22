7b8bbc9369c17d03329e5fe98f337e945a9940ff
prodiGamer1000 <168657105+prodiGamer1000@users.noreply.github.com>
2025-02-22 18:52:48 -0300

    Creating

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+
+let theme = "dark";
+
+function toggleTheme() {
+    if(theme == "light") {
+        theme = "dark"
+        moon.style.display = "none";
+        sun.style.display = "block";
+    } else {
+        theme = "light"
+        moon.style.display = "block";
+        sun.style.display = "none";
+    }
+} 
+
+
+
+setInterval(()=>{
+    
+},100)
