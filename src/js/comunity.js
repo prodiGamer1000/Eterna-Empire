0de9d92fe688eada8ab27b9fa3accd851cc51dd1
prodiGamer1000 <168657105+prodiGamer1000@users.noreply.github.com>
2025-02-24 19:58:39 -0300

    arrumando a aba comunidade no celular

--- src/js/comunity.js
+++ src/js/comunity.js
@@ -1,10 +1,18 @@
+alert("AVISO!! O site ainda está em desenvolvieno, portanto, algumas funções que funcionam em computadores podem não funcionar devidamente em celulares.");
+
 const sun = document.querySelector('.sun');
 const moon = document.querySelector('.moon');
 const main = document.querySelector('.main');
 const description = document.querySelector('.comunityDesc');
 const title = document.querySelector('.comunityTitle');
+const menuMobile = document.querySelector('.menuMobile');
+const menuMobileOpen = document.querySelector('.menuMobileOpen');
+const sunMobile = document.querySelector('.sunMobile');
+const moonMobile = document.querySelector('.moonMobile');
 
 let theme = "dark";
+let themeMobile = "dark";
+let menuMobilePressed = false;
 
 function toggleTheme() {
     if(theme == "light") {
@@ -17,9 +25,23 @@ function toggleTheme() {
         sun.style.display = "none";
     }
 } 
+function toggleThemeMobile(){
+    if(themeMobile == "light") {
+        themeMobile = "dark"
+        moonMobile.style.display = "none";
+        sunMobile.style.display = "block";
+    } else {
+        themeMobile = "light"
+        moonMobile.style.display = "block";
+        sunMobile.style.display = "none";
+    }
+}
 
 sun.addEventListener('click', ()=>{toggleTheme();});
 moon.addEventListener('click', ()=>{toggleTheme()});
+
+sunMobile.addEventListener('click', ()=>{toggleThemeMobile()});
+moonMobile.addEventListener('click', ()=>{toggleThemeMobile()});
 
 setInterval(()=>{
     if(theme == "light"){
@@ -34,3 +56,15 @@ setInterval(()=>{
         main.style.backgroundColor = "#222";
     }
 },50)
+
+menuMobile.addEventListener('click', ()=>{
+    if(menuMobilePressed) {
+        menuMobileOpen.style.display = 'none';
+        menuMobile.style.transform = "rotate(0deg)";
+        menuMobilePressed = false;
+    } else {
+        menuMobileOpen.style.display = 'flex';
+        menuMobile.style.transform = "rotate(90deg)";
+        menuMobilePressed = true;
+    }
+});
